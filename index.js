ada3657e22fb04673d3198582a3a5778707bf914
Mionne Gooch <mionne.gooch@outlook.com>
2021-08-11 16:15:40 -0400

    Final Project, Finished!!

--- index.js
+++ index.js
@@ -21,38 +21,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
     
     
     //smooth scroll
-    comment.onclick = function scroll(){
+    function scroll(){
         comment.scrollIntoView({behavior: "smooth", block:"start"});
     }
+    comment.addEventListener("click", scroll);
 
     
     // add to cart button that adds item to cart  
     //when add to cart is clicked, quantity innertext = 1, total innertext = price
-      cart.onclick =function addCart(){
+      function addCart(){
           count= parseInt(quantity.innerText);
           count++;
           quantity.innerText = parseInt(count);
           total.innerText = "$" + parseFloat(price*count);
           cartBtn.innerText = "My Cart ("+ count+ ")";
       }   
-    
+      cart.addEventListener("click",addCart);
     
 
     // checkout button needs to open modal close button needs to close modal
     //opens modal on my Cart button click
-    myCart.onclick = function openModal(){
+    function openModal(){
         modal.style.display = "block";
     }
+    myCart.addEventListener("click", openModal);
 
-    close.onclick = function closeModal(){
+    //close modal on close button
+    function closeModal(){
         modal.style.display = "none";
     }
+    close.addEventListener("click", closeModal);
 
     //quantity button needs to count up and down and show number
-
- 
-
-    up.onclick = function upQty(){
+    function upQty(){
         count = parseInt(quantity.innerText);
         count++;
         quantity.innerText= count;
@@ -61,7 +62,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         cartBtn.innerText = "My Cart ("+ count + ")";
     }
-    down.onclick = function downQty(){
+    up.addEventListener("click",upQty);
+    // down quantity
+    function downQty(){
         count = parseInt(quantity.innerText);
         count--;
         quantity.innerText= count;
@@ -70,29 +73,76 @@ document.addEventListener("DOMContentLoaded", (event) => {
         //my Cart in background update
         cartBtn.innerText = "My Cart ("+ count+ ")";
     }
- 
-    //empty cart reset count to zero reset price to $0 reset my Cart
+    down.addEventListener("click",downQty);
 
-    empty.onclick = function emptyCart(){
+
+    //empty cart reset count to zero reset price to $0 reset my Cart
+    function emptyCart(){
         count = parseInt(0);
         quantity.innerText = count;
         total.innerText = "$"+ parseFloat(price)*count;
         cartBtn.innerText = "My Cart ("+ count + ")";
-
-
     }
+    empty.addEventListener("click",emptyCart);
 
-    //checkout button brings up 
 
-    checkout.onclick = function chkModalOpen(){
+    //checkout button brings up modal
+    //open modal
+    function chkModalOpen(){
         closeModal();
-        // modal.style.display = "none";
         document.getElementById("chk-total").innerText = "Your Total is: "+ total.innerText;
         chkModal.style.display = "block"; 
     }
-
-    chkclose.onclick = function chkModalClose(){
+    checkout.addEventListener("click",chkModalOpen);
+    
+    //close modal
+    function chkModalClose(){
         chkModal.style.display = "none";
+        emptyCart();
     } 
+    chkclose.addEventListener("click", chkModalClose);
 
+    // leave comment functions
+    let newRow;
+    let newName;
+    let newComment;
+    let comTable = document.querySelector("tbody");
+    let comm = document.getElementById("comment");
+
+
+    function getName(){
+        let input;
+        let name = prompt("Please enter your name:","Anonymous");
+        if(name == null || name ==""){
+            input = "Submission Cancelled"
+        }else{
+            input = name;
+        }
+        return input;
+    }
+    function getComment(){
+        let input;
+        let comment = prompt("Please enter your comment", "Great Product");
+        if(comment == null || comment ==""){
+            input = "Submission Cancelled"
+        }else{
+            input = comment;
+        }
+        return input;
+    }
+    function revCom(){
+        newRow = document.createElement("tr");
+        newName = document.createElement("td");
+        newComment = document.createElement("td");
+
+        newName.innerText = getName();
+        newComment.innerText = getComment();
+
+        newRow.appendChild(newName);
+        newRow.appendChild(newComment);
+
+        comTable.appendChild(newRow);
+
+    }
+    comm.addEventListener("click", revCom)
 });
